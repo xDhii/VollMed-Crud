5a72b55d3145be3c6fd8fba7e4f397d7539d74d0
Adriano Araujo Valumin <adriano.valumin@outlook.com>
2024-03-31 17:39:28 -0300

    Implemented temporary logout button

--- Vollmed/Views/HomeView.swift
+++ Vollmed/Views/HomeView.swift
@@ -22,36 +22,60 @@ struct HomeView: View {
     }
 
     var body: some View {
-        ScrollView(showsIndicators: false) {
-            VStack {
-                Image(.logo)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 200)
-                    .padding(.vertical, 32)
-                Text("Boas-vindas!")
-                    .font(.title2)
-                    .bold()
-                    .foregroundStyle(Color(.lightBlue))
-                Text("Veja abaixo os especialistas da Vollmed disponíveis e marque já a sua consulta!")
-                    .font(.title3)
-                    .bold()
-                    .foregroundStyle(.accent)
-                    .multilineTextAlignment(.center)
-                    .padding(.vertical, 16)
-                ForEach(specialists) { specialist in
-                    SpecialistCardView(specialist: specialist)
-                        .padding(.bottom, 8)
+        NavigationStack {
+            ScrollView(showsIndicators: false) {
+                VStack {
+                    Image(.logo)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200)
+                        .padding(.vertical, 32)
+                    Text("Boas-vindas!")
+                        .font(.title2)
+                        .bold()
+                        .foregroundStyle(Color(.lightBlue))
+                    Text("Veja abaixo os especialistas da Vollmed disponíveis e marque já a sua consulta!")
+                        .font(.title3)
+                        .bold()
+                        .foregroundStyle(.accent)
+                        .multilineTextAlignment(.center)
+                        .padding(.vertical, 16)
+                    ForEach(specialists) { specialist in
+                        SpecialistCardView(specialist: specialist)
+                            .padding(.bottom, 8)
+                    }
+                }
+                .padding(.horizontal)
+            }
+            .padding(.top)
+            .onAppear {
+                Task {
+                    await getSpecialists()
                 }
             }
-            .padding(.horizontal)
         }
-        .padding(.top)
-        .onAppear {
-            Task {
-                await getSpecialists()
+
+        // MARK: - Logout Buttom
+
+        .safeAreaInset(
+            edge: .top,
+            alignment: .trailing,
+            content: {
+                Button(
+                    action: {
+                        UserDefaultsHelper.remove(for: "token")
+                        UserDefaultsHelper.remove(for: "patient-id")
+
+                    }, label: {
+                        Text("Logout")
+                            .bold()
+                            .foregroundStyle(.cancel)
+                            .padding(.horizontal)
+                            .padding(.bottom)
+                    }
+                )
             }
-        }
+        )
     }
 }
 
